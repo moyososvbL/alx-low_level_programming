0d288d7225bc6a60dda9876f94e7faba92fb9185
moyososvbL <oganlamoyosoreoluwa@gmail.com>
2022-09-08 16:19:37 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,29 +1,21 @@
 #include <stdio.h>
 
+/**
+ * main - prints the size of various types on the computer it is compiled and run on
+ *
+ * Return:0
+ */
+int main(void)
+{
 
+	int myInt;
+	float myFloat;
+	double myDouble;
+	char myChar;
 
-int main() {
-
-	 int myInt;
-
-	  float myFloat;
-
-	   double myDouble;
-
-	    char myChar;
-
-
-
-	     printf("%lu\n", sizeof(myInt));
-
-	      printf("%lu\n", sizeof(myFloat));
-
-	       printf("%lu\n", sizeof(myDouble));
-
-	        printf("%lu\n", sizeof(myChar));
-
-
-
-		  return 0;
-
+	printf("%lu\n", sizeof(myInt));
+	printf("%lu\n", sizeof(myFloat));
+	printf("%lu\n", sizeof(myDouble));
+	printf("%lu\n", sizeof(myChar));
+	return (0);
 }
