fcfcdb1ae62ff00bcaf21f92d3dceb79bf191535
moyososvbL <oganlamoyosoreoluwa@gmail.com>
2022-09-09 21:14:43 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,7 +13,7 @@ char last[] = "Last digit of";
 
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-printf("%s %d is %d and is ",last, n, n % 10);
+printf("%s %d is %d and is ", last, n, n % 10);
 if (n % 10 > 5)
 {
 printf("greater than 5\n");
@@ -24,7 +24,7 @@ printf("0\n");
 }
 else
 {
-printf("less tham 6 and not 0\n");
+printf("less than 6 and not 0\n");
 }
 return (0);
 }
