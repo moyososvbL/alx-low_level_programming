7f96fa9621c896664bbb0d339ae1ba0f6ebc9901
moyososvbL <oganlamoyosoreoluwa@gmail.com>
2022-09-13 15:17:25 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,34 @@
+#include "moyososvbL.h"
+
+/**
+ * main - Write a program that prints _putchar, followed by a new line
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	_putchar('m');
+	
+	_putchar('o');
+
+	_putchar('y');
+
+	_putchar('o');
+
+	_putchar('s');
+
+	_putchar('o');
+
+	_putchar('s');
+
+	_putchar('v');
+
+	_putchar('b');
+
+	_putchar('L');
+
+	_putchar('\n');
+
+return (0);
+
+}
