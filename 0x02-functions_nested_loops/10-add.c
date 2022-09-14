1c72795bf2b1ab964b80e69331b43d939ccbbe3d
moyososvbL <oganlamoyosoreoluwa@gmail.com>
2022-09-13 19:39:29 -0700

    10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,8 +2,8 @@
 
 /**
  * add - Adds two integers.
- * @moyo1: The first integer to be added.
- * @moyo2: The second integer to be added.
+ * @num1: The first integer to be added.
+ * @num2: The second integer to be added.
  *
  * Return: The result of the addition.
  */
