c0c54d7f19e33aeb57486a6e9ee147d96c4f062e
moyososvbL <oganlamoyosoreoluwa@gmail.com>
2022-09-13 17:43:48 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - Print "MoyososvbL" followed by a neqw line.
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	_putchar('M');
+	_putchar('o');
+	_putchar('y');
+	_putchar('o');
+	_putchar('s');
+	_putchar('o');
+	_putchar('s');
+	_putchar('v');
+	_putchar('b');
+	_putchar('L');
+
+	return (0);
+
+}
