a77fe2f847c1bb2e62e159384ee37e7c0443d7b0
moyososvbL <oganlamoyosoreoluwa@gmail.com>
2022-09-13 19:31:59 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main"
+
+/**
+ * add - Adds two integers.
+ * @moyo1: The first integer to be added.
+ * @moyo2: The second integer to be added.
+ *
+ * Return: The result of the addition.
+ */
+int add(int moyo1, int num2)
+{
+	return (moyo1 + moyo2);
+
+}
