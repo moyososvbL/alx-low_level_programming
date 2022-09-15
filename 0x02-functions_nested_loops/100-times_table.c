403a4a4b7887bd5ee480a1e504216c9febb3623b
moyososvbL <oganlamoyosoreoluwa@gmail.com>
2022-09-14 18:00:32 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -35,6 +35,23 @@ if (!(n > 15 || n < 0))
 
 	}
 
+	else if (res >= 10 && res < 100)
+
+	{
+		_putchar(' ');
+		_putchar((res / 10) + '0');
+		_putchar((res % 10) + '0');
+	}
+
+	 else if (res >= 100 && j != 0)
+	
+	{
+		 _putchar((res / 100) + '0');
+		 _putchar((res / 10) % 10 + '0');
+		 _putchar((res % 10) + '0');
+
+	}
+
 	else
 		_putchar((res % 10) + '0');
 
