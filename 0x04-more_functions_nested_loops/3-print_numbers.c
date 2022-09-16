daf672232fd7c4a026adb2fead779a8d13fb5813
moyososvbL <oganlamoyosoreoluwa@gmail.com>
2022-09-15 19:48:09 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,13 +2,13 @@
 
 /**
  * print_numbers - Prints the numbers from 0-9.
- *
  * Return: 0
  */
+
 void print_numbers(void)
 {
 int num;
-for (num = 48; num <= 58; num++)
+for (num = 0; num <= 9; num++)
 {
 _putchar(num);
 }
