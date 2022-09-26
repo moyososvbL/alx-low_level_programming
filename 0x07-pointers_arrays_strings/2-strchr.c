c3a43293984bd71422d0b3eb1e10cf78494b0a97
moyososvbL <oganlamoyosoreoluwa@gmail.com>
2022-09-26 14:33:48 -0700

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a character in a string
+ * @s: string pointer
+ * @c: string variable
+ *
+ * Return: first occurrence of c in string s or NULL if character is unfound.
+ */
+char *_strchr(char *s, char c)
+{
+	for (;; s++)
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		if (!*s)
+		{
+			return ('\0');
+		}
+	}
+	return ('\0');
+}
